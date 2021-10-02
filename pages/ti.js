1b6dabb1cd29ac945b0c76e95b3751536f8dc1f7
Pedro Emilio <pedroemiliokuhn@gmail.com>
2021-10-01 22:12:13 -0300

    testeti

--- pages/ti.js
+++ pages/ti.js
@@ -5,7 +5,7 @@ import ListagemVazia from '../components/listagem/listagemvazia';
 const categoria = "T.I."
 export const getServerSideProps = async (catogira) => {
     try {
-        const res = await fetch('https://servicostnn.vercel.app/api/getServicos', {
+        const res = await fetch('./api/getServicos', {
             method: 'POST',
             body: (categoria)
         })
